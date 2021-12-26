d1ed8bf3cfcba56b84770fd4513ea7821a18b920
mbanks94 <mbanks123456@gmail.com>
2021-12-25 21:52:36 -0600

    simplified theme context and provider

--- src/contexts/theme/index.tsx
+++ src/contexts/theme/index.tsx
@@ -1,30 +1,41 @@
-import { createContext, useContext, useReducer } from "react";
-import { IContext, IProviderProps } from "..";
-import { ThemeAction } from "./actions";
-import { initialState, themeReducer, ThemeState } from "./reducer";
+import { createContext, useContext, useState } from "react";
+import { IProviderProps } from "..";
+import { ThemeType } from "../../models";
 
-const ThemeContext = createContext<IContext<ThemeState, ThemeAction>>({
-    state: initialState,
-    dispatch: () => null,
-});
+interface IThemeContext {
+  theme: ThemeType;
+  toggleTheme: () => void;
+}
+
+const ThemeContext = createContext({} as IThemeContext);
 
 const ThemeProvider = ({ children }: IProviderProps) => {
-    const [state, dispatch] = useReducer(themeReducer, initialState);
+  const [theme, setTheme] = useState(ThemeType.LIGHT);
+  const toggleTheme = () => {
+    switch (theme) {
+      case ThemeType.LIGHT:
+        setTheme(ThemeType.DARK);
+        break;
+      case ThemeType.DARK:
+      default:
+        setTheme(ThemeType.LIGHT);
+        break;
+    }
+  };
 
-    return (
-        <ThemeContext.Provider value={{ state, dispatch }}>
-            {children}
-        </ThemeContext.Provider>
-    );
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
 };
 
 const useTheme = () => {
-    const context = useContext(ThemeContext);
-    if (context === undefined) {
-        throw new Error("Theme Context is undefined");
-    }
-    return context;
+  const context = useContext(ThemeContext);
+  if (context === undefined) {
+    throw new Error("Theme Context is undefined");
+  }
+  return context;
 };
 
-export { ThemeContext, ThemeProvider, useTheme };
-export { setThemeType } from "./actions";
+export { ThemeProvider, useTheme };
