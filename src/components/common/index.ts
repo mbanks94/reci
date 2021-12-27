94bf20a6eb4e1386913a9f6be1797a87d243c391
mbanks94 <mbanks123456@gmail.com>
2021-12-26 19:09:24 -0600

    added search to exports

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,6 +1,7 @@
 export { AddButton } from "./buttons/AddButton";
 export { Brand } from "./brand/Brand";
 export { ListView } from "./listview/ListView";
+export { SearchBar } from "./search/SearchBar";
+export { Spinner } from "./spinner/Spinner";
 export { ThemeToggle } from "./toggle/ThemeToggle";
 export { Toggle } from "./toggle/Toggle";
-export { Spinner } from "./spinner/Spinner";
