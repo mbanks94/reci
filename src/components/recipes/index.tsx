b456fb425b1cb6a2f1c635ff9184db1fa4d4e3d3
mbanks94 <mbanks123456@gmail.com>
2022-01-02 22:37:53 -0600

    handle recipes state

--- src/components/recipes/index.tsx
+++ src/components/recipes/index.tsx
@@ -1,14 +1,33 @@
 import { RecipeBox } from "./RecipeBox";
 import { AddButton } from "../common";
+import { useRecipes } from "../../contexts/recipes";
+import { useEffect, useState } from "react";
+import { Recipe } from "../../models";
+import { RecipeCard } from "./RecipeCard";
 
 export const Recipes = () => {
+  const { getRecipes, recipeState } = useRecipes();
+  const [recipes, setRecipes] = useState<Recipe[]>([]);
+
+  useEffect(() => {
+    getRecipes();
+  }, [getRecipes]);
+
+  useEffect(() => {
+    setRecipes(recipeState.recipes);
+  }, [recipeState]);
+
   const addRecipe = () => {
     console.log("add");
   };
 
   return (
     <>
-      <RecipeBox />
+      <RecipeBox>
+        {recipes.map((recipe, i) => {
+          return <RecipeCard key={i} id={i} recipe={recipe} />;
+        })}
+      </RecipeBox>
       {/* <div className="add-container mt-50">
         <AddButton onClick={addRecipe} text="Add a recipe" />
       </div> */}
